b038711100089860713fbeeec6648887ee873b9d
William Butts <wbutts1023@gmail.com>
2015-10-15 14:52:27 -0400

    registration set up.  Basic enter site view set up.

--- urlybird/crisco/views.py
+++ urlybird/crisco/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username,password=user.password)
+            login(request,user)
+            return redirect('home_page', request.user.pk)
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form':form})
+
+    def start(request):
+        if user.is_authenticated():
+            redirect('home', request.user.pk)
+        else:
+            redirect('all')
