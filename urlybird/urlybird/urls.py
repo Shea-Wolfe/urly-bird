73389ec950228aac6d80627b81192a0f4809fa7a
William Butts <wbutts1023@gmail.com>
2015-10-15 13:42:47 -0400

    think the built in login is working

--- urlybird/urlybird/urls.py
+++ urlybird/urlybird/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+       {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 ]
