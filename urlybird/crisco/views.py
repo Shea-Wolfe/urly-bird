63fa9ee12fd31fcb3e52fe445cf05ddab07445ff
Ryan Burton <rryanburton@gmail.com>
2015-10-16 00:22:33 -0400

    fixed some model names and added imports

--- urlybird/crisco/views.py
+++ urlybird/crisco/views.py
@@ -4,27 +4,31 @@ from django.contrib.auth import authenticate, login
 from django.views.generic import ListView
 from .models import Bookmark
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
+
 
 def register_user(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user = authenticate(username=user.username,password=user.password)
-            login(request,user)
+            user = authenticate(username=user.username, password=user.password)
+            login(request, user)
             return redirect('home_page', request.user.pk)
 
     else:
         form = UserCreationForm()
-    return render(request, 'crisco/register.html', {'form':form})
+    return render(request, 'crisco/register.html', {'form': form})
+
 
 def start(request):
-    if user.is_authenticated():
+    if request.user.is_authenticated():
         redirect('home_page', request.user.username)
     else:
         redirect('recent')
+
 
 class AllBookmarks(ListView):
     '''Generic ListView that will show the 20 most recent bookmarks.
@@ -36,6 +40,7 @@ class AllBookmarks(ListView):
     def get_queryset(self):
         preload = Bookmark.objects.all().select_related('user')
         return preload.order_by('-modified')
+
 
 class UserPage(ListView):
     '''Generic ListView that will show the 20 most recent bookmarks a user posted.'''
@@ -59,12 +64,14 @@ class HomePage(ListView):
         self.user = get_object_or_404(User, username=self.kwargs['pk'])
         self.user.bookmark_set.all().order_by('-modified')
 
+
 @login_required
 def delete_bookmark(request, bookmark_id):
     if Bookmark.objects.get(pk=bookmark_id).user == request.user:
         Bookmark.objects.get(pk=bookmark_id).delete()
         messages.add_message(request, messages.SUCCESS, "Bookmark deleted!")
-        return redirect('home_page',rater_id=request.user.username)
+        return redirect('home_page', rater_id=request.user.username)
     else:
-        messages.add_message(request, ERROR, "You can't delete what is not yours!")
+        messages.add_message(
+            request, messages.ERROR, "You can't delete what is not yours!")
         return redirect('recent')
