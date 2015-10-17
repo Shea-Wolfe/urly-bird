03b104ede8c090131f205992d3974c75e7e03454
William Butts <wbutts1023@gmail.com>
2015-10-17 17:58:12 -0400

    fixed pagination.  added a bookmark generator to manage.py commands

--- urlybird/crisco/management/commands/generate_users.py
+++ urlybird/crisco/management/commands/generate_users.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from crisco.models import Bookmark
 
 
 def generate_users():
@@ -10,6 +11,21 @@ def generate_users():
     for _ in range(100):
         user = User.objects.create_user(user_list[count],password='password',email=fake.email())
         user.save()
+        count += 1
+
+def generate_bookmarks():
+    from faker import Faker
+    import random
+    fake = Faker()
+    count = 1
+    for _ in range(100):
+        bookmark = Bookmark(title='bookmark{}'.format(count),
+                            comment='Comment{}'.format(count),
+                            longurl=fake.url(),
+                            modified=fake.date_time_this_year(),
+                            user=random.choice(User.objects.all()))
+        bookmark.generate_short()
+        bookmark.save()
         count += 1
 
 class Command(BaseCommand):
