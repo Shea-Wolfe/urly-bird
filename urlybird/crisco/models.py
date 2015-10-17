181ccdf9a8cd6db4ba7ffed089646c402a336f7a
Ryan Burton <rryanburton@gmail.com>
2015-10-17 11:40:55 -0400

    added def generate_short(self) to Bookmark.models

--- urlybird/crisco/models.py
+++ urlybird/crisco/models.py
@@ -14,14 +14,22 @@ class Bookmark(models.Model):
     def __str__(self):
         return "{}".format(self.title)
 
-    # def generate_short(self):
-    #     '''Generates a shorturl for the bookmark.
-    #     also validates to make sure it doesn't already exist'''
-    #     short = generator
-    #     if Bookmark.objects.filter(shorturl=short).count() == 1:
-    #         self.generate_short()
-    #     else:
-    #         self.shorturl = short
+    def generate_short(self):
+        '''Generates a shorturl for the bookmark.
+        also validates to make sure it doesn't already exist.
+        N = number of characters.
+        string.ascii_uppercase + string.digits = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        '''
+        import string
+        import random
+        N = 6
+        short = ''.join(random.SystemRandom().choice(
+            string.ascii_uppercase + string.digits) for _ in range(N))
+        if Bookmark.objects.filter(shorturl=short).count() == 1:
+            self.generate_short()
+        else:
+            self.shorturl = short
+
 
 class Click(models.Model):
     timestamp = models.DateTimeField()
